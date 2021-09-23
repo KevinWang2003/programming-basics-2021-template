3f7f1e2977e6298152c77985ff3dfa07827a738a
KevinWang2003 <wang0071@hz.nl>
2021-09-23 11:09:37 +0200

    My own file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-console.log('JavaScript is working!')
+const firstName = "Kevin";
+const lastName = "Wang";
+const age = "17";
+const Adjective = "Mad";
+let a = 5, b = 6;
+let c = (a**2+b**2)**0.5;
+
+
+
+
+let priceOfAnApple = '0.59';
+let numberOfApples = 200;
+
+let total = priceOfAnApple*numberOfApples;
+
+
+console.log(`Hello, I am ${firstName} ${lastName} and I am ${age} years old`);
+console.log(`${total}`)
+console.log(`${c}`)
+console.log(`Please visit the HZ University of ${Adjective} Sciences`)
